2da41b99428a8b0b553330fd64b91de35871626a
Ztellar <b.salamanca01@ufromail.cl>
2023-05-04 09:16:30 -0400

    Adición de los métodos y ejecución del Main

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Calculadora {
+	private int n1;
+	private int n2;
+
+	public Calculadora() {
+		this.n1 = 0;
+		this.n2 = 0;
+	}
+	public Calculadora(int num1, int num2) {
+		this.n1 = num1;
+		this.n2 = num2;
+	}
+	public int sumar() {
+		return this.n1 + this.n2;
+	}
+	public int multiplicar() {
+		return this.n1*this.n2;
+	}
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+}
